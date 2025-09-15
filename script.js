aea99d757db9b37f9b1e1ba851ab21d49ae30210
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-09-15 17:14:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
-
+	event.preventDefault(); 
+	let form=event.target;
+	let firstName=form["fname"].value.trim();
+	let lastName=form["lname"].value.trim();
+	alert(firstName+" "+lastName);
 }
